9563b3956956d1e10e2fbc40724cab52dcbea1c4
ijh701016 <ijh701016@ajou.ac.kr>
2024-12-29 17:37:58 +0900

    update: 2주차 과제 완료

--- src/App.js
+++ src/App.js
@@ -1,93 +1,9 @@
 import "./App.css";
 
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+import { Detailpage } from "./pages/Detailpage";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      <div>aaaaaaaaaaaaaaaaaaaa</div>
-      {/* 여기에 바디 */}
-      <Footer />
-    </div>
-  );
+  return <Detailpage />;
 }
 
 export default App;
